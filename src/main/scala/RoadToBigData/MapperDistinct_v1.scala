578c79f7b6868103fa17633b0f602507d486e795
t.zalimov <t.zalimov@deltasolutions.ru>
2018-10-30 14:16:55 +0300

    Решил задачку 4.1.5

---
+++ src/main/scala/RoadToBigData/MapperDistinct_v1.scala
@@ -0,0 +1,17 @@
+package RoadToBigData
+
+import scala.io.StdIn
+
+object MapperDistinct_v1 {
+  def main(args: Array[String]) {
+    var line = ""
+    while ( {
+      line = StdIn.readLine();
+      line != null
+    }) {
+      val subArray = line.trim.split("\t")
+      val (key, value) = (line.trim.split("\t")(0), line.trim.split("\t")(1).split(","))
+      for (token <- value) if (key != null) println(key + "," + token + "\t" + "1")
+    }
+  }
+}
