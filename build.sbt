d40c0d11ad20f7d80740b5fee54eb70cfbbf3325
t.zalimov <t.zalimov@deltasolutions.ru>
2018-11-08 18:35:23 +0300

    Пробный запуск

--- build.sbt
+++ build.sbt
@@ -11,7 +11,8 @@ libraryDependencies ++= Seq(
   "org.apache.hbase" % "hbase-common" % "1.2.2",
   "org.apache.hbase" % "hbase-client" % "1.2.2",
   "org.apache.hadoop" % "hadoop-common" % "2.7.1",
-  "org.apache.hbase" % "hbase-rest" % "2.1.1"
+  "org.apache.hbase" % "hbase-rest" % "2.1.1",
+  "com.hortonworks" % "shc-core" % "1.1.1-2.1-s_2.11"
 )
 
 
--- src/main/scala/BigDataLabs/OneExtended.scala
+++ src/main/scala/BigDataLabs/OneExtended.scala
@@ -6,7 +6,7 @@ object OneExtended {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Spark Program").setMaster("local")
     val spark = new SparkContext(conf)
-    val file = spark.textFile("C:\\Users\\timur\\Desktop\\Вводная_BigData\\input_file_1.txt")
+    val file = spark.textFile("C:\\Users\\Cronium\\Desktop\\Вводная_BigData\\input_file_1.txt")
     val counts = file.flatMap(_.split("\\s+"))
       .filter(_.matches("^[А-яA-Za-z]+[,\\.]?$"))
       .map(word => if (word.matches("^.*[,\\.]$")) (word.dropRight(1), 1) else (word, 1))
