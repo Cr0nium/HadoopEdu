07633c9d5d4351acbaa0fd6e8cd05b73f90af357
t.zalimov <t.zalimov@deltasolutions.ru>
2018-11-08 17:14:53 +0300

    Добавил зависимостей

--- build.sbt
+++ build.sbt
@@ -5,4 +5,18 @@ version := "0.1"
 scalaVersion := "2.11.8"
 
 // https://mvnrepository.com/artifact/org.apache.spark/spark-core
-libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.3.2"
+libraryDependencies ++= Seq(
+  "org.apache.spark" % "spark-core_2.11" % "2.3.2",
+  "org.apache.hbase" % "hbase-server" % "1.2.2",
+  "org.apache.hbase" % "hbase-common" % "1.2.2",
+  "org.apache.hbase" % "hbase-client" % "1.2.2",
+  "org.apache.hadoop" % "hadoop-common" % "2.7.1",
+  "org.apache.hbase" % "hbase-rest" % "2.1.1"
+)
+
+
+
+
+
+
+
---
+++ src/main/scala/BigDataLabs/StackTest.scala
@@ -0,0 +1,34 @@
+package BigDataLabs
+
+import org.apache.hadoop.hbase.client.{Get, Put, ResultScanner, Scan}
+import org.apache.hadoop.hbase.rest.client.{Client, RemoteHTable}
+import org.apache.hadoop.hbase.util.Bytes
+
+
+object StackTest {
+  def main(args: Array[String]) {
+    val hbaseCluster  = new org.apache.hadoop.hbase.rest.client.Cluster()
+    hbaseCluster.add("192.168.6.20", 7180)
+    val restClient = new Client(hbaseCluster)
+    val table =  new RemoteHTable(restClient, "TestWordCount")
+    println("connected...")
+    var p = new Put(Bytes.toBytes("row1"))
+    p.addColumn(Bytes.toBytes("0"), Bytes.toBytes("NAME"),Bytes.toBytes("raju"))
+    p.addColumn(Bytes.toBytes("0"), Bytes.toBytes("COURSE"),Bytes.toBytes("SCALA"))
+    p.addColumn(Bytes.toBytes("0"), Bytes.toBytes("YEAR"),Bytes.toBytes("2017"))
+    table.put(p)
+
+    val scan = new Scan()
+    val scanner : ResultScanner = table.getScanner(scan)
+    println("got scanner...")
+    val g = new Get(Bytes.toBytes("row1"))
+    val result = table.get(g)
+
+    val name = Bytes.toString(result.getValue(Bytes.toBytes("0"),Bytes.toBytes("NAME")))
+    val course = Bytes.toString(result.getValue(Bytes.toBytes("0"),Bytes.toBytes("COURSE")))
+    val year = Bytes.toString(result.getValue(Bytes.toBytes("0"),Bytes.toBytes("YEAR")))
+
+    println("row1 " + "name: " + name + " course: " + course + " year:" + year);
+
+  }
+}
