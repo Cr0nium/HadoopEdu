1a82934dbe1133bfda4b9fa27eb4b9b0e3ab4fd2
t.zalimov <t.zalimov@deltasolutions.ru>
2018-11-12 18:43:15 +0300

    Попытка добовления файла построчно в Hbase

--- src/main/scala/Demo/HbaseReadOne.scala
+++ src/main/scala/Demo/HbaseReadOne.scala
@@ -1,8 +1,10 @@
 package Demo
 
 import org.apache.hadoop.hbase.client._
+import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.hbase.{HBaseConfiguration, HColumnDescriptor, HTableDescriptor, TableName}
 import org.apache.spark._
+import org.apache.spark.rdd.RDD
 
 
 object HbaseReadOne {
@@ -12,18 +14,42 @@ object HbaseReadOne {
     val conf = HBaseConfiguration.create()
 
     conf.set("hbase.master", "192.168.6.20:7180")
-    conf.setInt("timeout", 120000)
-    conf.set("hbase.zookeeper.quorum", "192.168.6.20")
-    conf.set("zookeeper.znode.parent", "/hbase")
+    conf.setInt("timeout", 60000)
+    conf.set("hbase.zookeeper.quorum", "192.168.6.21")
+
     val conn = ConnectionFactory.createConnection(conf)
     val admin = conn.getAdmin
+    val table = conn.getTable(TableName.valueOf(Bytes.toBytes("TestWordCount")))
+    val file: RDD[String] = sc.textFile("C:\\Users\\Cronium\\Desktop\\Вводная_BigData\\input_file_1.txt")
+    getClass.getResource("input_file_1.txt")
+    scala.io.Source.fromFile("input_file_1.txt").getLines()
+    HBaseContext
 
-    val hTableDesc = new HTableDescriptor(TableName.valueOf("Customer2"))
-    hTableDesc.addFamily(new HColumnDescriptor("name"))
-    hTableDesc.addFamily(new HColumnDescriptor("contactinfo"))
-    hTableDesc.addFamily(new HColumnDescriptor("address"))
+
+
+    /* =================  CREATE TABLE "TestWordCount"  =======================
+    val hTableDesc = new HTableDescriptor(TableName.valueOf("TestWordCount"))
+    hTableDesc.addFamily(new HColumnDescriptor("data"))
     admin.createTable(hTableDesc)
+    */
 
+    var put = new Put(Bytes.toBytes("row3"))
+    put.addColumn(Bytes.toBytes("data"), Bytes.toBytes("text"), Bytes.toBytes(file.toString()))
+    table.put(put)
+
+    val result = table.get(new Get(Bytes.toBytes("row2")))
+    val r1 = result.getValue(Bytes.toBytes("data"), Bytes.toBytes("text"))
+    val resultText = result.getValue(Bytes.toBytes("data"), Bytes.toBytes("text"))
+    val newFile = Bytes.toString(r1)
+    println(newFile)
+
+/*
+    val counts = newFile.flatMap(_.split("\\s+"))
+      .filter(_.matches("^[А-яA-Za-z]+[,\\.]?$"))
+      .map(word => if (word.matches("^.*[,\\.]$")) (word.dropRight(1), 1) else (word, 1))
+      .reduceByKey(_ + _)
+    counts.saveAsTextFile("CountSaveFile")
+*/
     sc.stop()
   }
 }
